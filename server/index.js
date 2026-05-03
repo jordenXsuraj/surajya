0454e3a5e422207804c0d4b21f8ff3f2591c6398
Suraj Mote <surajbhivajimote@gmail.com>
2026-05-03 12:38:26 +0530

    fix cors for deployment

--- server/index.js
+++ server/index.js
@@ -92,6 +92,6 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log(`✅ Server running: http://localhost:${PORT}`)
+  console.log(`✅ Server running`)
   console.log(`✅ Mode: ${process.env.NODE_ENV}`)
 })
